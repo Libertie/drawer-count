4459a701d7e6ca75642831d5c4ceafbe15113880
Libertie <745156+Libertie@users.noreply.github.com>
2020-02-13 14:06:28 -0500

    Modal history details

--- app/views/partials/modal.view.php
+++ app/views/partials/modal.view.php
@@ -1,3 +1,7 @@
+<?php
+$currency_index = $currency->getDenominations(true);
+
+?>
 <div id="history-modal" class="modal" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -28,13 +32,46 @@
                         aria-labelledby="heading-<?= $drawer['id'] ?>"
                         data-parent="#accordion">
                             <div class="card-body">
-                                <?= $drawer['total'] ?><br />
-                                <?= $drawer['details'] ?>
+                                <p>
+                                    Total: <?= $currency->format($drawer['total']) ?> 
+                                    <?php if ($drawer['expected']) : ?>
+                                    <br>Expected: <?= $currency->format($drawer['expected']) ?>
+                                    <br>Discrepancy: 
+                                    <?= $currency->format($drawer['discrepancy']) ?>
+                                    <?php endif; ?>
+                                </p>
+                                <table class="table table-sm">
+                                    <thead>
+                                        <tr>
+                                            <th>Denomination</th>
+                                            <th class="text-center">Qty.</th>
+                                            <th class="text-right">Ext.</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach (json_decode($drawer['details'], true) as $type => $denominations) : ?>
+                                        <?php if (array_sum($denominations)) : ?>
+                                        <tr><td colspan="3" class="text-muted text-uppercase pt-3">
+                                            <small><?= ucwords($type) ?></small>
+                                        </td></tr>
+                                        <?php foreach ($denominations as $denomination => $qty) : ?>
+                                            <tr>
+                                                <td><?= $currency_index[$type][$denomination] ?></td>
+                                                <td class="text-center"><?= $qty ?: '0' ?></td>
+                                                <td class="text-right">
+                                                    <?= $currency->format(($qty ? ($qty * $denomination / 100) : 0)) ?>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach; ?>
+                                        <?php endif; ?>
+                                        <?php endforeach; ?>
+                                    </tbody>
+                                </table>
                             </div>
                     </div>
                 </div>
                 <?php endforeach; ?>
-                <?= ! count($drawers) ? '<div class="modal-body">There are no previously recorded counts.</div>' : '' ?> 
+                <?= !count($drawers) ? '<div class="modal-body">There are no previously recorded counts.</div>' : '' ?> 
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
